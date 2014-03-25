5106e119645d6772285562282c38a63d75601abc
crazySocket <michal.pletty@gmail.com>
2014-03-25 01:41:14 +0100

    singleLoop and loop added. However, I am not sure where to put points from the cluster to be deleted so that part I dummy for now.

--- src/Hartigan.cpp
+++ src/Hartigan.cpp
@@ -12,11 +12,36 @@ Hartigan(int numberOfClusters, float killThreshold,
 }
 
 void Hartigan::loop() {
-
+  while(singleLoop() > 0);
 }
 
-void Hartigan::singleLoop() {
+int Hartigan::singleLoop() {
+  int switched = 0;
+  for(int i = 0; i < Cluster::numberOfPoints; i++) {
+    source = fits[i];
+    NumericVector point = points(i,_);
+    for(int k = 0; k < clusters.size(); k++)
+      if(k != source && clusters[k].size > 0) {
+	Cluster &oldSource = clusters[source];
+	Cluster &oldTarget = cluster[k];
+	Cluster newSource = clusters[source].removePoint(point);
+	Cluster newTarget = clusters[k].addPoint(point);
+	float oldEntropy = oldSource.entropy()+oldTarget.entropy();
+	float newEntropy = newSource.entropy()+newTarget.entropy();
+	if(newEntropy < oldEntropy) {
+	  cluster[source] = newSource;
+	  cluster[k] = newTarget;
+	  switched++;
 
+	  if(cluster[source].size < killThreshold*Cluster::numberOfPoints) {
+	    //put elemenents into other clusters
+	    //delete cluster
+	    //but how excactly?
+	  }
+	}
+      }
+  }
+  return switched;
 }
 
 double Hartigan::energy() {
