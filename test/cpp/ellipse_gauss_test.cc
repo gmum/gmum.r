78617c516a963beb9418d8386435a87e14595b37
IIUJ-MaciejZgliczynski <zglicz@gmail.com>
2014-04-07 10:20:59 +0200

    test for ellipse gauss

--- test/cpp/ellipse_gauss_test.cc
+++ test/cpp/ellipse_gauss_test.cc
@@ -1,5 +1,7 @@
 #include "gtest/gtest.h"
 #include "cluster_reader.hpp"
+#include "clustering_comparator.hpp"
+#include "src/Hartigan.hpp"
 #include <vector>
 
 TEST(EllipseGauss,answer_cluster_same_length) {
@@ -17,7 +19,40 @@ TEST(EllipseGauss,answer_cluster_same_length) {
   EXPECT_EQ(points.size(), clustering.size());
   EXPECT_EQ(points.size(), m.n_rows);
   EXPECT_EQ(points[0].size(), m.n_cols);
+  BestPermutationComparator comparator;
+  EXPECT_EQ(comparator.evaluateClustering(4,m,clustering,clustering),1.0);
   
+}
+
+TEST(EllipseGauss,real_test){ 
+  std::vector<unsigned int> clustering;
+  
+  ClusterReader clusterReader("EllipseGauss",2);
+  unsigned int numberOfClusters = 4;
+  //clusterReader.readClustering();
+  //clusterReader.readPoints();
+  arma::mat m = clusterReader.getPointsInMatrix();
+  clusterReader.getClustering(clustering);
+  BestPermutationComparator comparator;
+  
+  for (int x = 0 ; x < 20 ; ++x) {
+    int seed = time(NULL);
+    srand(seed);
+  
+    std::vector<unsigned int> fits;
+    fits.reserve(clustering.size());
+
+    for(unsigned int i = 0; i < m.n_rows; i++) fits[i] = rand()%numberOfClusters;
+
+    Hartigan algorithm(numberOfClusters,0.0001, fits, m);
+    
+    algorithm.loop();
+    std::vector<unsigned int> result;
+    algorithm.getFits(result);
+    double percentage = comparator.evaluateClustering(numberOfClusters,m,result,clustering);
+    std::cout << "Percentage " << percentage << std::endl;
+    EXPECT_GT(percentage, 0.9);
+  }
 }
 
 
