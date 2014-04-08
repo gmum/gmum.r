970f20231428b3a729090dd865b259aa0f7bde5a
crazySocket <michal.pletty@gmail.com>
2014-04-08 12:33:27 +0200

    1) run divided into smaller functions. 3) assign uses reserve/push_back, type,covMat,radius use resize/[] problem: project compiles but doesn't load 'Błąd w dyn.load(dllfile)'

--- src/HartiganMaster.hpp
+++ src/HartiganMaster.hpp
@@ -6,3 +6,14 @@
 #include "CEC.hpp"
 #include <list>
 #include <vector>
+
+RcppExport SEXP run(SEXP args);
+
+void initClusters(std::list<Rcpp::List> &clusters, Rcpp::List &list);
+
+void initVectors(std::vector<ClusterType> &type, 
+		 std::vector<arma::mat> &covMat,
+		 std::vector<float> &radius,
+		 std::list<Rcpp::List> clusters);
+
+void initAssignRandom(std::vector<unsigned int> &assignment, int nrOfPoints, int nrOfClusters);
