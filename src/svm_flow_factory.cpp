1a5152759d0edcaad9364b622ae7036fdaae3279
igorsieradzki <igor@Desktop.(none)>
2014-04-09 21:14:44 +0200

    Divide code in multiple files and added parameters and data structures

---
+++ src/svm_flow_factory.cpp
@@ -0,0 +1,9 @@
+#include "svm_flow_factory.h"
+#include <vector>
+
+// SVMFlowFactory | At the moments returns empty vector<hanlder>
+std::vector<SVM_Handler*> SVM_FlowFactory::createSVMFlow( SVM_Configuration config ) {
+  std::vector<SVM_Handler*> handlers;
+  return handlers;
+  // TODO
+}
