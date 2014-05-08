1628f82f13ae872a7f66ae38d12306c97ba1d54a
IIUJ-MaciejZgliczynski <zglicz@gmail.com>
2014-05-08 14:08:02 +0200

    Made the generator static. So now it is initialized only once. Looks a lot more random

--- src/random_assignment.cpp
+++ src/random_assignment.cpp
@@ -7,8 +7,8 @@
 void initAssignRandom(std::vector<unsigned int> &assignment,
 		      unsigned int nrOfPoints,
 		      unsigned int nrOfClusters) {
-  int seed = time(NULL);
-  boost::random::mt19937 gen(seed);
+  static int seed = time(NULL);
+  static boost::random::mt19937 gen(seed);
   boost::random::uniform_int_distribution<> dist(0,nrOfClusters-1);
   assignment.reserve(nrOfPoints);
 
