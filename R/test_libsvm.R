772ccd7f9b1c31edbb722318dd98768ff11c8c7c
igorsieradzki <sieradzki.igor@gmail.com>
2014-06-04 19:30:42 +0200

    Wrapper clean-up

--- R/test_libsvm.R
+++
@@ -1,8 +0,0 @@
-#' Description
-#' Testing architecture flow
-#' Details
-#' On success it should print out the argument.
-#' @export
-test_libsvm <- function(){
-	.Call( "test_libsvm", PACKAGE = "gmum.r" )
-}
