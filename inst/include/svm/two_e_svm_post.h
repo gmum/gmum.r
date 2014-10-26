a4c0bfb5fadda1c80023429c93423d774964038f
Igor Sieradzki <sieradzki.igor@gmail.com>
2014-10-26 18:40:54 +0100

    Revert "Fixed merge, enchancent tests makefile"

--- inst/include/svm/two_e_svm_post.h
+++ inst/include/svm/two_e_svm_post.h
@@ -1,15 +1,13 @@
-#ifndef __TWOESVM_H_POST__
-#define __TWOESVM_H_POST__
-
 #include "svm_handler.h"
 #include "svm_basic.h"
 #include <RcppArmadillo.h>
 
 class TwoeSVMPostprocessor: public SVMHandler {
-public:
+private:
 	arma::rowvec projectingData(arma::mat &matrix, arma::rowvec &weights);
+	void makePreprocessor();
+	float shiftingBoundary();
+public:
 	void processRequest(SVMConfiguration&);
 	bool canHandle(SVMConfiguration&);
 };
-
-#endif
