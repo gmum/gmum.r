f6c4cf171f27bfa0afdd7f7bbd75a9a9469ee0b0
kjurek <kjurek123@gmail.com>
2014-10-29 20:33:48 +0100

    added tests for mouse_1 data set

--- tests/testthat/test_entropy_simple.R
+++ tests/testthat/test_entropy_simple.R
@@ -12,12 +12,3 @@ test_that("Entropy is correct", {
 
     expect_that(actual_energy, equals(expected_energy, tolerance = 1e-4))
 })
-
-test_that("Entropy is correct with initial centroids", {
-    centroids = dataset_clusters - min(dataset_clusters)
-    args = list(k=1, x=dataset_points, params.init='centroids', params.centroids = centroids)
-    c <- CEC(args)
-   	actual_energy <- c$entropy()
-	
-	expect_that(actual_energy, equals(expected_energy, tolerance = 1e-4))
-})
