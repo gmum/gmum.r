e7f752c79926dd8493126747b76f1268161be61e
igorsieradzki <sieradzki.igor@gmail.com>
2014-10-30 14:45:37 +0100

    inlined plot, install doesnt work

--- demo/svm_demo.R
+++ demo/svm_demo.R
@@ -1,7 +1,5 @@
 library('gmum.r')
 
-# DEPRECATED !!!
-
 # Load a dataset, here we have provided an example 
 ds <- svm.breast_cancer()
 
@@ -15,31 +13,9 @@ svm <- SVM( formula = X1~. ,
             gamma = 0.01,
             coef0 = 0) 
 
-
-
-# # All functions are generic, meaning you nedd to provide your model as a first argument
-# load_dataset(svm, ds)
-
 # You can access the dataset 
 x <- dataset.X(svm)
 y <- dataset.Y(svm)
-
-# # You can change the dataset, but that requires class methods
-# svm$setX(x)
-# svm$setY(y)
-# 
-# # You can change model paramaters using params function for specific parameter
-# params.C(svm, 100)
-# 
-# # or change multiple parameters at once
-# params(svm, C=1, gamma=0.01)
-# 
-# # To view specific parameter you can use the same function without a values, or print a summary for your model
-# params.C(svm)
-# print(svm)
-# 
-# # Use train(object) function to train your model on provided dataset
-# train(svm)
 
 # Classify your dataset using predict function
 prediction <- predict(svm, x)
