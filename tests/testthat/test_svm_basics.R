b3825657e0a886cfbe0c4a248a8f6f6e107519c7
igorsieradzki <sieradzki.igor@gmail.com>
2014-11-04 15:12:18 +0100

    rewritten tests, doc, demo for new api

---
+++ tests/testthat/test_svm_basics.R
@@ -0,0 +1,47 @@
+library(testthat)
+library('gmum.r')
+
+test_that('SVM fucntions is fool proof', {
+  
+  ds = svm.dataset.2e()
+  f = V3 ~ .
+  
+  expect_error( SVM(f, ds, lib="xyz"), "No such library. Avaiable are: libsvm" )
+  expect_error( SVM(f, ds, kernel="xyz"), "No such kernel type. Avaiable are: linear, poly, rbf, sigmoid")
+  expect_error( SVM(f, ds, prep="xyz"), "No such preprocess type, Avaiable are: 2e, none" )
+  expect_error( SVM(f, ds, C = -1), "Wrong parameters" )
+  expect_error( SVM(f, ds, gamma = -1), "Wrong parameters" )
+  expect_error( SVM(f, ds, degree = 0), "Wrong parameters" )
+  
+  expect_warning( SVM(f, ds, kernel="linear", gamma=1), "Gamma parameter is not used with linear kernel" )
+  expect_warning( SVM(f, ds, kernel="linear", degree=3), "Degree parameter is not used with linear kernel" )
+  
+})
+print("test::SVM error")
+
+test_that('formulas and data storing works', {
+  
+  dataset <- svm.dataset.breast_cancer()
+  x1 = data.matrix( dataset[,names(dataset) != "X1"] )
+  y1 = data.matrix( dataset[,"X1"] )
+  svm <- SVM( X1 ~ ., dataset )
+  x2 = svm$getX()
+  y2 = svm$getY()
+  expect_that(all.equal(x1,x2,check.attributes=FALSE), is_true())
+  expect_that(all.equal(y1,y2,check.attributes=FALSE), is_true())
+  
+  formula = X1 ~ X2 + X3 + X4
+  data = all.vars(update(formula,0~.))
+  x3 = data.matrix( dataset[,data] )
+  y3 = data.matrix( dataset[,"X1"] )
+  svm2 <- SVM(formula, dataset)
+  x4 = svm2$getX()
+  y4 = svm2$getY()
+  expect_that(all.equal(x3,x4,check.attributes=FALSE), is_true())
+  expect_that(all.equal(y3,y4,check.attributes=FALSE), is_true())
+  
+})
+print("test::SVM formula and dataset")
+
+
+
