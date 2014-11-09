032af3b8ec135944e595e2ab49bc8c55f81a754a
marcinMarcin <marcin.gorlice@gmail.com>
2014-11-09 21:09:39 +0100

    Random assignment in tests fixed

--- tests/cpp/cec/mouse_1_spherical_test.cc
+++ tests/cpp/cec/mouse_1_spherical_test.cc
@@ -41,6 +41,7 @@ TEST_F(Mouse1SphericalTest,IsEnergyCorrect) {
   for (int i = 0 ; i < t ; ++i) {
     boost::shared_ptr<std::vector<unsigned int> > assignment(new std::vector<unsigned int>());
     RandomAssignment randomAssignment(*points, params.nrOfClusters);
+    assignment->resize(params.dataset->n_rows);
     randomAssignment(*assignment);
     boost::shared_ptr<Hartigan> hartigan(new Hartigan(false,false));
 //    std::vector<ClusterType> types;
