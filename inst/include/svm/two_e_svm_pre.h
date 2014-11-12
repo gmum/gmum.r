76172b17ddf4ad454ca4fcb7a343775c4cb3e993
sacherus <sacherus@gmail.com>
2014-11-12 02:34:12 +0100

    Working 2e. Again.

--- inst/include/svm/two_e_svm_pre.h
+++ inst/include/svm/two_e_svm_pre.h
@@ -1,5 +1,5 @@
-#ifndef TWOESVM_H
-#define TWOESVM_H
+#ifndef __TWOESVM_H_PRE__
+#define __TWOESVM_H_PRE__
 
 #include "svm_handler.h"
 #include "svm_basic.h"
@@ -11,12 +11,13 @@ class TwoeSVMPreprocessor: public SVMHandler {
 private:
     arma::mat computeTransMat(arma::mat &covPosMat, arma::mat &covNegMat);
     void sqrtInvMat(const arma::mat &matrix, arma::mat &finalMat);
-    arma::mat cov0InvSqrt;
+
 
 public:
     void processRequest(SVMConfiguration&);
     bool canHandle(SVMConfiguration&);
     void makePreprocessor();
+    arma::mat cov0InvSqrt;
 };
 
 #endif
