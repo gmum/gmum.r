27e804fafa61684f8071b8623c3906370738c8b5
kudkudak <staszek.jastrzebski@gmail.com>
2014-11-20 18:28:16 +0100

    Added tests. Tests are so broken..

---
+++ tests/cpp/cec/ClusterTest.cc
@@ -0,0 +1,28 @@
+#include "gtest/gtest.h"
+#include <armadillo>
+#include "src/Cluster.hpp"
+using  namespace gmum;
+class ClusterTest : public ::testing::Test {
+protected:
+  virtual void SetUp() {
+    c = Cluster();
+  }
+
+  Cluster c;
+};
+
+TEST_F(ClusterTest, MeanTest) {
+  arma::mat dataset(4,5);
+  dataset.row(0) = arma::rowvec("1 2 3 4 5");
+  dataset.row(1) = arma::rowvec("10 20 30 40 50");
+  dataset.row(2) = arma::rowvec("1 2 3 4 5");
+  dataset.row(3) = arma::rowvec("1 2 3 4 5");
+
+  std::vector<int> fits(4);
+  fits.push_back(1);
+  fits.push_back(2);
+  fits.push_back(1);
+  fits.push_back(1);
+
+  //ASSERT_EQ(arma::rowvec("1 2 3 4 5"), c.initializeMean(1,fits,dataset));
+}
---
+++ tests/cpp/cec/big_data_tests.cc
@@ -0,0 +1,105 @@
+#include "gtest/gtest.h"
+#include "cluster_reader.hpp"
+#include "src/Hartigan.hpp"
+#include "src/CEC.hpp"
+#include <vector>
+#include <armadillo>
+#include <boost/smart_ptr.hpp>
+#include "src/random_assignment.hpp"
+using namespace gmum;
+
+#define SHOW(x) std::cout << #x << " = " << x << std::endl
+int times = 10;
+void run(const char * str, int numberOfClusters, int times = 1) {
+  std::cout.precision(41);
+  boost::shared_ptr<std::vector<unsigned int> > clustering(new std::vector<unsigned int>());
+  ClusterReader clusterReader(str,4);
+  clusterReader.getClustering(*clustering);
+
+  int min = *(std::min_element(clustering->begin(),clustering->end()));
+  for (std::vector<unsigned int>::iterator it = clustering->begin(); it!= clustering->end() ; ++it)
+    *it -= min;
+boost::shared_ptr<arma::mat> points(new arma::mat(clusterReader.getPointsInMatrix()));
+  double killThreshold = 0.0001;
+ for (int i = 0 ; i < times ; ++i) {
+    boost::shared_ptr<std::vector<unsigned int> > assignment(new std::vector<unsigned int>());
+    initAssignRandom(*assignment, points->n_rows, numberOfClusters);
+    boost::shared_ptr<Hartigan> hartigan(new Hartigan(false,false));
+    CEC cec(points, assignment, hartigan, killThreshold, numberOfClusters);
+
+    cec.loop();
+    std::cout << "Energy " << cec.entropy() << std::endl;
+ }
+ ASSERT_TRUE(true);
+  
+}
+
+  void runSpherical(const char * str, int numberOfClusters, int times = 1){
+      std::cout.precision(41);
+  boost::shared_ptr<std::vector<unsigned int> > clustering(new std::vector<unsigned int>());
+  ClusterReader clusterReader(str,4);
+  clusterReader.getClustering(*clustering);
+
+  int min = *(std::min_element(clustering->begin(),clustering->end()));
+  for (std::vector<unsigned int>::iterator it = clustering->begin(); it!= clustering->end() ; ++it)
+    *it -= min;
+boost::shared_ptr<arma::mat> points(new arma::mat(clusterReader.getPointsInMatrix()));
+  double killThreshold = 0.0001;
+      std::vector<ClusterType> types;
+    std::vector<float> radius;
+    std::vector<arma::mat> covMatrices;
+    for (int i = 0 ; i < numberOfClusters ; ++i){
+      types.push_back(sphere);
+    }
+ for (int i = 0 ; i < times ; ++i) {
+    boost::shared_ptr<std::vector<unsigned int> > assignment(new std::vector<unsigned int>());
+    initAssignRandom(*assignment, points->n_rows, numberOfClusters);
+    boost::shared_ptr<Hartigan> hartigan(new Hartigan(false,false));
+    CEC cec(points, assignment, hartigan, killThreshold, types,radius,covMatrices);
+
+    cec.loop();
+    std::cout << "Energy " << cec.entropy() << std::endl;
+ }
+ ASSERT_TRUE(true);
+  }
+TEST(BigData,Normal_1) {
+  
+  run("bigData_1",3,times);
+}
+
+
+TEST(BigData,Normal_2) {
+  
+  run("bigData_2",3,times);
+}
+
+TEST(BigData,Normal_3) {
+  
+  run("bigData_3",3,times);
+}
+
+TEST(BigData,Normal_4) {
+  
+  run("bigData_4",3,times);
+}
+
+TEST(BigData,Spherical_1) {
+  
+  runSpherical("bigData_1",3,times);
+}
+
+
+TEST(BigData,Spherical_2) {
+  
+  runSpherical("bigData_2",3,times);
+}
+
+TEST(BigData,Spherical_3) {
+  
+  runSpherical("bigData_3",3,times);
+}
+
+TEST(BigData,Spherical_4) {
+  
+  runSpherical("bigData_4",3,times);
+}
