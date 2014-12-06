f2c47e5ed14593d5a2ed75f85779839050e89c3a
Mateusz Bruno-Kaminski <m.bruno.kaminski@gmail.com>
2014-12-06 17:54:15 +0100

    gh-pages branch update
    Link to the GMUM webpage, update of the building photo
    Small update of the team section & CSS
    Update of the team section & authors page
    Small update of the team section
    Refreshment of the team section & publications
    CEC document, team section on the main page

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,22 @@
-$(document).ready(function () {/* activate scrollspy menu */
+$(document).on('ready', function() {
+    /* activate scrollspy menu */
     $('body').scrollspy({
         target: '#navbar-collapsible',
         offset: 50
     });
 
     /* smooth scrolling sections */
-    $('a[href*=#]:not([href=#])').click(function () {
+    $('a[href*=#]:not([href=#])').on('click', function() {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
             if (target.length) {
-                $('html,body').animate({
+                $('html, body').animate({
                     scrollTop: target.offset().top - 50
                 }, 1000);
+
                 return false;
             }
         }
     });
-
 });
