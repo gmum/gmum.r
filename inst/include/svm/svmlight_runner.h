8edb318d26bb8bdf7d8f87cff1704d986aeb0ab5
Konrad Talik <konradtalik@gmail.com>
2014-12-11 18:48:16 +0100

    SVMLIGHT
    Refers to #73

---
+++ inst/include/svm/svmlight_runner.h
@@ -0,0 +1,135 @@
+/**
+ * @file        svmlight_runner.h
+ * @brief       SVMLight implementation class of SVMHandler headers
+ * @author      Konrad Talik <konradtalik@gmail.com>
+ * @copyright   GPLv3
+ */
+
+#ifndef SVMLIGHT_RUNNER_H
+#define SVMLIGHT_RUNNER_H
+
+#include "svm_handler.h"
+
+
+extern "C" {
+#include "svmlight/svm_common.h"
+#include "svmlight/svm_learn.h"
+}
+#define SVMLightRunner_temp_model_file "svmlightrunner_temp_model_file"
+#define SVMLightRunner_temp_output_file "svmlightrunner_temp_output_file"
+
+/// SVMLight library implementation of SVMHandler
+class SVMLightRunner: public SVMHandler {
+
+public:
+
+    /// Default constructor
+    SVMLightRunner();
+
+    /// Default destructor
+    ~SVMLightRunner();
+
+    // Documented in the parent class
+    bool canHandle(SVMConfiguration &);
+
+    // Documented in the parent class
+    void processRequest(SVMConfiguration &);
+
+protected:
+    
+    // TODO: Move this method to SVMHandler?
+    void predict(SVMConfiguration &);
+
+    /** @name Library functionalities wrappers
+     *  Following methods are direct library functionalities wrappers with
+     *  file operations parametrized (`bool use_gmumr`).
+     */
+    /// @{
+
+    /**
+     * SVMLight's `svm_learn` main method
+     * @author  Thorsten Joachims
+     */
+    int librarySVMLearnMain(int argc, char **argv, bool use_gmumr,
+        SVMConfiguration &config
+    );
+
+    /**
+     * SVMLight's `svm_learn` auxiliary method
+     * @author  Thorsten Joachims
+     */
+    void librarySVMLearnReadInputParameters(
+        int argc, char *argv[], char *docfile, char *modelfile,
+        char *restartfile, long *verbosity, LEARN_PARM *learn_parm,
+        KERNEL_PARM *kernel_parm, bool use_gmumr, SVMConfiguration &config
+    );
+
+    /**
+     * SVMLight's `svm_classify` main method
+     * @author  Thorsten Joachims
+     */
+    int librarySVMClassifyMain(int argc, char **argv, bool use_gmumr,
+        SVMConfiguration &config
+    );
+
+    /**
+     * SVMLight's `svm_classify` auxiliary method
+     * @author  Thorsten Joachims
+     */
+    void librarySVMClassifyReadInputParameters(
+        int argc, char **argv, char *docfile, char *modelfile,
+        char *predictionsfile, long int *verbosity, long int *pred_format,
+        bool use_gmumr, SVMConfiguration &config
+    );
+
+    /**
+     * SVMLight's auxiliary method
+     * @author  Thorsten Joachims
+     */
+    MODEL * libraryReadModel(
+        char *modelfile, bool use_gmumr, SVMConfiguration &config
+    );
+
+    /**
+     * SVMLight's auxiliary method
+     * @author  Thorsten Joachims
+     */
+    void libraryReadDocuments(
+        char *docfile, DOC ***docs, double **label, long int *totwords,
+        long int *totdoc, bool use_gmumr, SVMConfiguration &config
+    );
+
+    /// @}
+
+    /**
+     * Convert SVMConfiguration to one line of SVMLight's `svm_learn` input
+     */
+    char * SVMConfigurationToSVMLightLearnInputLine(
+        SVMConfiguration &config, long int line_num
+    );
+
+    /**
+     * Convert SVMConfiguration to one line of SVMLight's `svm_classify` input
+     * of every Support Vector in one line (starting with alpha*y)
+     */
+    char * SVMConfigurationToSVMLightModelSVLine(
+        SVMConfiguration &config, long int line_num
+    );
+
+    /**
+     * Convert SVMConfiguration to SVMLight's model file lines
+     */
+    char ** SVMConfigurationToSVMLightModelFile(SVMConfiguration &config);
+
+    /**
+     * Store SVMLight model data into SVMConfiguration
+     * NOTE: It does not clean unnecessary data!
+     */
+    void SVMLightModelToSVMConfiguration(
+        MODEL *model, SVMConfiguration *config
+    );
+};
+
+/* SVMLIGHT_RUNNER_H */
+#endif
+
---
+++ tests/cpp/svm/svmlight_test.cpp
@@ -0,0 +1,112 @@
+#include <armadillo>
+#include <iostream>
+#include <string>
+
+#include "gtest/gtest.h"
+#include "svmlight_runner.h"
+#include "svm_basic.h"
+
+namespace {
+
+// The fixture for testing class SVMLightRunner
+class SVMLightRunnerTest: public ::testing::Test {
+
+protected:
+
+    SVMLightRunnerTest() {
+        svmLightRunner = SVMLightRunner();
+
+        learing_data_01
+            << 0.5 << 1.0 << 0.0 << 1.0 << arma::endr
+            << 0.4 << 1.1 << 0.0 << 1.0 << arma::endr
+            << 0.5 << 0.9 << 1.0 << 0.0 << arma::endr
+            << 0.5 << 1.0 << 1.0 << 0.0 << arma::endr
+            << 0.4 << 1.1 << 1.0 << 0.0 << arma::endr;
+        learing_target_01 << -1 << -1 << 1 << 1 << 1;
+
+        testing_data_01
+            << 0.4 << 0.9 << 0.0 << 1.0 << arma::endr
+            << 0.5 << 0.9 << 0.0 << 1.0 << arma::endr
+            << 0.4 << 1.0 << 1.0 << 0.0 << arma::endr
+            << 0.5 << 1.0 << 1.0 << 0.0 << arma::endr;
+        testing_target_01 << -1 << -1 << 1 << 1;
+    }
+
+    virtual ~SVMLightRunnerTest() {}
+
+    /// Called immediately after the constructor (righ before each test)
+    virtual void SetUp() {}
+
+    /// Called immediately after each test (right before the destructor)
+    virtual void TearDown() {}
+
+    /* Objects declared here can be used by all tests in the test case */
+
+    SVMLightRunner svmLightRunner;
+
+    arma::mat learing_data_01;
+    arma::vec learing_target_01;
+    arma::mat testing_data_01;
+    arma::vec testing_target_01;
+};
+
+
+} // end namespace
+
+/* Fixture tests */
+
+TEST_F(SVMLightRunnerTest, processRequest_learning) {
+    std::cout << "SVMLightRunner..." << std::endl;
+    SVMLightRunner svmlr;
+    std::cout << "SVMConfiguration..." << std::endl;
+    SVMConfiguration svm_config;
+
+    std::cout << "SVMConfiguration data..." << std::endl;
+    svm_config.data = learing_data_01;
+    std::cout << "SVMConfiguration target..." << std::endl;
+    svm_config.target = learing_target_01;
+    std::cout << "SVMConfiguration setPrediction..." << std::endl;
+    svm_config.setPrediction(false);
+    std::cout << "Processing request." << std::endl;
+    svmlr.processRequest(svm_config);
+
+    // kernel_type - LINEAR
+    ASSERT_EQ(svm_config.kernel_type, 0);
+    // -d int      -> parameter d in polynomial kernel
+    ASSERT_EQ(svm_config.degree, 3);
+    // -g float    -> parameter gamma in rbf kernel
+    ASSERT_EQ(svm_config.gamma, 1);
+    // -s float    -> parameter s in sigmoid/poly kernel
+    ASSERT_EQ(svm_config.coef0, 1);
+    // -r float    -> parameter c in sigmoid/poly kernel
+    ASSERT_EQ(svm_config.C, 1);
+    // -u string   -> parameter of user defined kernel
+    ASSERT_EQ(std::string(svm_config.kernel_parm_custom), std::string("empty"));
+    // highest feature index - no assignment to read-only data
+    ASSERT_EQ(svm_config.data.n_cols, 4);
+    // number of support vectors
+    ASSERT_EQ(svm_config.l, 5);
+    // threshold b
+    ASSERT_DOUBLE_EQ(svm_config.threshold_b, -0.11450359507913976);
+}
+
+TEST_F(SVMLightRunnerTest, processRequest_classification) {
+    SVMLightRunner svmlr;
+    SVMConfiguration svm_config;
+
+    svm_config.data = learing_data_01;
+    svm_config.target = learing_target_01;
+    svm_config.setPrediction(false);
+    svmlr.processRequest(svm_config);
+
+    svm_config.data = testing_data_01;
+    svm_config.target = testing_target_01;
+    svm_config.setPrediction(true);
+    svmlr.processRequest(svm_config);
+
+    ASSERT_DOUBLE_EQ(svm_config.result[0], -0.77104032841091008);
+    ASSERT_DOUBLE_EQ(svm_config.result[1], -0.77104080978434542);
+    ASSERT_DOUBLE_EQ(svm_config.result[2], 0.99999861699854542);
+    ASSERT_DOUBLE_EQ(svm_config.result[3], 0.9999981356251102);
+}
+
