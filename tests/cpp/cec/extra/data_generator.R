76150a2795b310e59095d165697aa421dd7e2204
kjurek <kjurek123@gmail.com>
2015-01-15 21:58:19 +0100

    changed generator to generate 2 gausses

--- tests/cpp/cec/extra/data_generator.R
+++ tests/cpp/cec/extra/data_generator.R
@@ -1,7 +1,9 @@
 data_path = './data/'
 for(i in seq(100, 10000, by=100))
 {
-    data = rnorm(i, mean = 3, sd = 2)
+    gauss_1 = rnorm(i / 2, mean = 0, sd = 1)
+    gauss_2 = rnorm(i / 2, mean = 2, sd = 0.8)
+    data = c(gauss_1, gauss_2)
     filename = paste(c(data_path, i,'.RData'), collapse="")
     write.table(data, file=filename, row.names=FALSE, col.names=FALSE)    
 }
