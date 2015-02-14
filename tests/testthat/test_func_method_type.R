ff5aed6846b46a947afa4248718b27c5c108ac07
marcinMarcin <marcin.gorlice@gmail.com>
2015-02-14 11:22:30 +0100

    Test func method type with devtools problem fixed/

--- tests/testthat/test_func_method_type.R
+++ tests/testthat/test_func_method_type.R
@@ -6,11 +6,11 @@ data(cec_simple_1)
 expected_energy <- energy_value
 dataset_points <- input
 
-f_standard <- function(m, sigma){
-  return (m * log(2 * pi * exp(1)) / 2 + log(det(sigma)) / 2)
-}
-
-test_that("Entropy is correct", {
+test_that("Entropy is correct", {  
+  f_standard <- function(m, sigma){
+    return (m * log(2 * pi * exp(1)) / 2 + log(det(sigma)) / 2)
+  }
+  
   c1 <- CEC(k=1, x=dataset_points, method.type='func', params.function='f_standard')
   c2 <- CEC(k=1, x=dataset_points, method.type='standard')
 	
