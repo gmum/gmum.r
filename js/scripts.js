c5cfd436809e16b9e4d5fa35b985a5dd842aac07
Mateusz Bruno-Kaminski <m.bruno.kaminski@gmail.com>
2015-03-28 00:03:12 +0100

    Repository history video, section regarding TFML
    Concerns #334

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 $(document).on('ready', function() {
-    /* activate scrollspy menu */
+    // Activates scrollspy menu
     $('body').scrollspy({
         target: '#navbar-collapsible',
         offset: 50
     });
 
-    /* smooth scrolling sections */
+    // Smooth scrolling sections
     $('a[href*=#]:not([href=#])').on('click', function() {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
@@ -20,7 +20,23 @@ $(document).on('ready', function() {
         }
     });
 
+    // Activates highlight.js
     $('pre code').each(function(i, block) {
         hljs.highlightBlock(block);
+    });
+
+    // Controls repository history video
+    var videoPosition = $('#repository-history').position().top,
+        video = $('#repository-history video').get(0);
+    video.pause();
+    $(window).on('scroll', function() {
+        var scrollTop = $(this).scrollTop(),
+            topOffset = videoPosition - (0.7 * $(this).height()),
+            bottomOffset = videoPosition + 310;
+        if (scrollTop >= topOffset && scrollTop <= bottomOffset) {
+            video.play();
+        } else {
+            video.pause();
+        }
     });
 });
