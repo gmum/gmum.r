76805a491f8471cbe87334e35147f73df8f1e01b
Mateusz Bruno-Kaminski <m.bruno.kaminski@gmail.com>
2015-03-28 13:10:38 +0100

    Repository history video update
    Concerns #334

--- js/scripts.js
+++ js/scripts.js
@@ -24,19 +24,4 @@ $(document).on('ready', function() {
     $('pre code').each(function(i, block) {
         hljs.highlightBlock(block);
     });
-
-    // Controls repository history video
-    var videoPosition = $('#repository-history').position().top,
-        video = $('#repository-history video').get(0);
-    video.pause();
-    $(window).on('scroll', function() {
-        var scrollTop = $(this).scrollTop(),
-            topOffset = videoPosition - (0.7 * $(this).height()),
-            bottomOffset = videoPosition + 310;
-        if (scrollTop >= topOffset && scrollTop <= bottomOffset) {
-            video.play();
-        } else {
-            video.pause();
-        }
-    });
 });
