c5959cf64559cadb662c29ce0a73c960a08f73c4
marcinMarcin <marcin.gorlice@gmail.com>
2015-04-10 21:56:47 +0200

    CEC summary improvement

--- R/cec.summary.R
+++ R/cec.summary.R
@@ -13,9 +13,26 @@ summary.cec <- NULL
 
 evalqOnLoad({  
   summary.cec <<- function(object) {
-    print.cec(object)    
+    print.cec(object)
+  
+    if(isParameterOn(object$log.iters())){
+      print("Iterations: ")
+      print(object$log.iters())
+    }
+    if(isParameterOn(object$log.energy())){
+      print("Energy for every iteration: ")
+      print(object$log.energy())
+    }
+    if(isParameterOn(object$log.ncluster())){
+      print("Number of clusters for every iteration: ")
+      print(object$log.ncluster())
+    }
   }
   
   setMethod("summary", "Rcpp_CecModel", summary.cec)
   setMethod("show", "Rcpp_CecModel", summary.cec)
 })
+
+isParameterOn <- function(x) {
+  return(length(x) != 0)
+}
