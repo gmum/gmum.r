0c6cb10455b852b35e699c14590ef984aac331b0
Konrad Talik <konradtalik@gmail.com>
2015-04-13 15:11:43 +0200

    Task #392 startup

--- tests/testthat/test_svm_sparse.R
+++ tests/testthat/test_svm_sparse.R
@@ -36,21 +36,25 @@ test_that("sparse matrices work", {
   x <- read.matrix.csr(system.file("inst", "data_sets", "svm", "dexter_train.data", mustWork=TRUE, package="gmum.r"))
   y <- read.table(system.file("inst", "data_sets", "svm", "dexter_train.labels", mustWork=TRUE, package="gmum.r"))
 
-  liblist <- c("svmlight", "libsvm")
+    libs <- c("svmlight", "libsvm")
+    kernels <- c("linear", "poly", "rbf", "sigmoid")
 
-  for (lib_i in liblist) {
+    for (lib_i in libs) {
+        for (kernel_i in kernels) {
+            print("")
+            print(sprintf("Kernel %s", kernel_i))
+            print(sprintf("Testing %s", lib_i))
 
-      print(sprintf("Testing %s", lib_i))
+            print("Learning...")
+            svm <- SVM(x=x, y=y, lib=lib_i, kernel=kernel_i, C=1, verbosity=0)
 
-      print("Learning...")
-      svm <- SVM(x=x, y=y, lib=lib_i, kernel="linear", C=1, verbosity=0)
+            print("Prediction...")
+            pred <- predict(svm, x)
 
-      print("Prediction...")
-      pred <- predict(svm, x)
-
-      target <- svm$getY()
-      acc <- svm.accuracy(prediction=pred, target=target)
-      print(sprintf("Sparse acc, %s prediction: %f", lib_i, acc))
+            target <- svm$getY()
+            acc <- svm.accuracy(prediction=pred, target=target)
+            print(sprintf("Sparse acc, %s prediction: %f", lib_i, acc))
+        }
   }
 
 })
