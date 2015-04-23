03fb7598ce809302c7e483f98ebe2a17b549d276
igorsieradzki <sieradzki.igor@gmail.com>
2015-04-23 15:48:07 +0200

    tests hotfix

--- tests/testthat/test_svm_class_and_example_weights.R
+++ tests/testthat/test_svm_class_and_example_weights.R
@@ -31,7 +31,7 @@ ex_weights_svm <- SVM(formula=y~., data=df, lib="svmlight", kernel="linear", C=1
 cl_weights_svm <- SVM(formula=y~., data=df, lib="svmlight", kernel="linear", C=1.0, class.weights=cl_weights)
 ex_cl_weights_svm <- SVM(formula=y~., data=df, lib="svmlight", kernel="linear", C=1.0, class.weights=cl_weights, example.weights=ex_weights)
 
-ex_SV <- ex_weights_svm$get_SV()
+ex_SV <- ex_weights_svm$getSV()
 
 print(length(ex_SV))
 print(length(sk_ex_SV))
