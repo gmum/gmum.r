711f22d1369e1d9d5c4096b7e8d77dbe8c78b326
kudkudak <staszek.jastrzebski@gmail.com>
2015-04-27 11:36:14 +0200

    #448 GNG sample

---
+++ demo/samples/gng.mouse.R
@@ -0,0 +1,39 @@
+library(gmum.r)
+library(ggplot2)
+
+# Load our library dataset
+data(cec.mouse.1.spherical)
+
+# Train GNG model and find centroids of the resulting graph
+g <- GNG(cec.mouse.1.spherical, max.nodes=50)
+
+# GNG aims at making it easy to work with its graph.
+# Here we will predict closest centroid 
+mouse.centr <- centroids(g)
+
+# Now we can plot results to see decision boundary for assigning node to centroid
+m = as.data.frame(cec.mouse.1.spherical)
+colnames(m) = c("x", "y")
+
+x_col <- cec.mouse.1.spherical[,1]
+y_col <- cec.mouse.1.spherical[,2]
+
+x_max <- max(x_col)
+x_min <- min(x_col) 
+y_max <- max(y_col)
+y_min <- min(y_col)
+
+x_axis <- seq(from=x_min, to=x_max, length.out=30)
+y_axis <- seq(from=y_min, to=y_max, length.out=30)
+grid <- data.frame(x_axis,y_axis)
+grid <- expand.grid(x=x_axis,y=y_axis)
+
+target <- predictCentroid(g, centroids=mouse.centr, x=grid)
+grid["target"] <- target
+
+
+pl <- ggplot()+ 
+  geom_tile(data=grid, aes(x=x,y=y, fill=factor(target))) + theme(legend.position="none") +
+  geom_point(data=m, aes(x,y), color='white') + scale_size_continuous(range = c(3, 6))
+plot(pl)
+
---
+++ demo/samples/gng.optimized.R
@@ -0,0 +1,22 @@
+# For advanced used you might want to consider using OptimizedGNG
+# , which asymptotically much faster implementation without
+# sacrifying quality of the graph
+
+# GNG unique capability is online training
+
+library(gmum.r)
+
+# Construct 10^6 points spherical dataset. Might take a while!
+sphere.dataset <- gng.preset.sphere(N=1000000)
+
+# If you decide to use this advanced feature, you have to pass
+# range in which all passed data will reside
+gng <- OptimizedGNG(sphere.dataset, max.nodes=1000, max.iter=10000, dim=3, 
+                    value.range=c(0,1))
+
+# GNG would much longer, you can check it yourself
+# gng <- GNG(sphere.dataset, max.nodes=1000, max.iter=10000, dim=3)
+
+
+# Plot results
+plot(gng, vertex.size=6)
