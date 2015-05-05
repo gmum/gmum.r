c8fdb610b5f6b8f581d0314a3de362b441df5eed
kjurek <kjurek123@gmail.com>
2015-05-05 23:28:45 +0200

    fixed 'there are degenerated clusters' spam

--- src/cec/cec.cpp
+++ src/cec/cec.cpp
@@ -158,11 +158,20 @@ void CecModel::find_best_cec() {
             init_clusters(assignment);
             loop();
 
+            if(!std::isnormal(m_result.energy))
+            {
+                continue;
+            }
+            
             if (m_result.energy < best_cec.get_result().energy) {
                 best_cec = *this;
             }
         }
         *this = best_cec;
+        if(!std::isnormal(m_result.energy))
+        {
+            GMUM_WARNING("There are degenerated clusters! You should try run CEC with other parameters")
+        }
     } catch (std::exception &e) {
 #ifdef RCPP_INTERFACE
         Rcpp::stop(std::string("exception ") + e.what() + " caught in CEC_new");
