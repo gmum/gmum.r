6c5650c3ca6ba4a4f5f00f18d7f8a291a3ffc961
kjurek <kjurek123@gmail.com>
2015-05-17 13:35:56 +0200

    added missing datasets

--- tests/testthat/test_cec_t_set.R
+++ tests/testthat/test_cec_t_set.R
@@ -2,7 +2,7 @@ library(testthat)
 library('gmum.r')
 
 test_that("spherical works", {
-  data(cec_t_set)
+  data(cec.tset)
   c <- CEC(x = Tset, k = 10, method.type = 'spherical', control.nstart = 100, control.eps = 0.07)
   plot(c, ellipses = TRUE, centers = TRUE)
   
@@ -13,7 +13,7 @@ test_that("spherical works", {
 })
 
 test_that("fixed_spherical works", {
-  data(cec_t_set)
+  data(cec.tset)
   c <- CEC(x = Tset, k = 10, method.type = 'fixed_spherical', params.r = 0.01, control.nstart = 100, control.eps = 0.07)
   plot(c, ellipses = TRUE, centers = TRUE)
   
@@ -24,7 +24,7 @@ test_that("fixed_spherical works", {
 })
 
 test_that("diagonal works", {
-  data(cec_t_set)
+  data(cec.tset)
   c <- CEC(x = Tset, k = 10, method.type = 'diagonal', control.nstart = 100, control.eps = 0.1)
   plot(c, ellipses = TRUE, centers = TRUE)
   
@@ -35,7 +35,7 @@ test_that("diagonal works", {
 })
 
 test_that("fixed_covariance works", {
-  data(cec_t_set)
+  data(cec.tset)
   c <- CEC(x = Tset, k = 10, method.type = 'fixed_covariance', params.cov=matrix(c(0.04, 0, 0, 0.01), 2), control.nstart = 100, control.eps = 0.07)
   plot(c, ellipses = TRUE, centers = TRUE)
   
@@ -46,7 +46,7 @@ test_that("fixed_covariance works", {
 })
 
 test_that("mix works", {
-  data(cec_t_set)
+  data(cec.tset)
   fixed_spherical_cluster_param = list(method.type = 'fixed_spherical', params.r = 0.001)
   fixed_covariance_cluster_param = list(method.type = 'fixed_covariance', params.cov=matrix(c(0.05, 0, 0, 0.001), 2))
   c <- CEC(x = Tset, k = 4, params.mix = list(fixed_covariance_cluster_param, fixed_spherical_cluster_param, fixed_spherical_cluster_param,
