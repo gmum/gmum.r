c15ab94607455a044c0cd418e6c36e5ec968b829
Konrad Talik <konradtalik@gmail.com>
2015-06-07 01:03:08 +0200

    Bunch class wrap

--- python/gmumpy/datasets.py
+++ python/gmumpy/datasets.py
@@ -1,15 +1,23 @@
 """
 Datasets module
 """
-import os
 import numpy as np
-from sklearn.datasets.base import Bunch
+import os
+import sklearn.datasets.base
 
-DATA_HOME=os.path.abspath(os.path.join(os.path.dirname(__file__), '../../inst/data_sets'))
+DATA_HOME = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../inst/data_sets'))
+
+
+# TODO: Consider creating `datasets` package TODO that in gmumpy.datasets.base
+class Bunch(sklearn.datasets.bunch):
+
+    pass
+
 
 # TODO
 def get_data_home():
     return DATA_HOME
+
 
 def load_simple1():
     simple1_path = os.path.join(DATA_HOME, 'cec', 'simple_1')
@@ -19,6 +27,7 @@ def load_simple1():
     bunch['k'] = np.unique(bunch.target).size
     return bunch
 
+
 def load_mouse1():
     mouse1_path = os.path.join(DATA_HOME, 'cec', 'mouse_1')
     bunch = Bunch(name='mouse1')
@@ -27,8 +36,9 @@ def load_mouse1():
     bunch['k'] = np.unique(bunch.target).size
     return bunch
 
+
 def load_ellipse_gauss():
-    ellipse_gauss_path = os.path.join(DATA_HOME, 'cec', 'EllipseGauss') 
+    ellipse_gauss_path = os.path.join(DATA_HOME, 'cec', 'EllipseGauss')
     bunch = Bunch(name='ellipse_gauss')
     bunch['data'] = np.loadtxt(os.path.join(ellipse_gauss_path, 'input.txt'))
     bunch['target'] = np.loadtxt(os.path.join(ellipse_gauss_path, 'cluster.txt'))
