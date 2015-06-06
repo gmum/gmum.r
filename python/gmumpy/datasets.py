6403f573c58452a7a9de74ba6babc8aae27110a6
kjurek <kjurek123@gmail.com>
2015-06-06 16:05:15 +0200

    added cec datasets to datasets.py, implemented CEC class constructor

--- python/gmumpy/datasets.py
+++ python/gmumpy/datasets.py
@@ -1,6 +1,37 @@
 """
 Datasets module
 """
+import os
+import numpy as np
+from sklearn.datasets.base import Bunch
+
+DATA_HOME=os.path.abspath(os.path.join(os.path.dirname(__file__), '../../inst/data_sets'))
 
 # TODO
+def get_data_home():
+    return DATA_HOME
+
+def load_simple1():
+    simple1_path = os.path.join(DATA_HOME, 'cec', 'simple_1')
+    bunch = Bunch(name='simple1')
+    bunch['data'] = np.loadtxt(os.path.join(simple1_path, 'input.txt'))
+    bunch['target'] = np.loadtxt(os.path.join(simple1_path, 'cluster.txt'))
+    bunch['k'] = np.unique(bunch.target).size
+    return bunch
+
+def load_mouse1():
+    mouse1_path = os.path.join(DATA_HOME, 'cec', 'mouse_1')
+    bunch = Bunch(name='mouse1')
+    bunch['data'] = np.loadtxt(os.path.join(mouse1_path, 'input.txt'))
+    bunch['target'] = np.loadtxt(os.path.join(mouse1_path, 'cluster.txt'))
+    bunch['k'] = np.unique(bunch.target).size
+    return bunch
+
+def load_ellipse_gauss():
+    ellipse_gauss_path = os.path.join(DATA_HOME, 'cec', 'EllipseGauss') 
+    bunch = Bunch(name='ellipse_gauss')
+    bunch['data'] = np.loadtxt(os.path.join(ellipse_gauss_path, 'input.txt'))
+    bunch['target'] = np.loadtxt(os.path.join(ellipse_gauss_path, 'cluster.txt'))
+    bunch['k'] = np.unique(bunch.target).size
+    return bunch
 
--- python/test.py
+++ python/test.py
@@ -2,11 +2,14 @@
 
 import numpy as np
 from sklearn.datasets import load_svmlight_file
-
+from gmumpy.datasets import load_mouse1
 from gmumpy.svm import SVMConfiguration
+from gmumpy.cec import CEC
 
 X, y = load_svmlight_file('../inst/data_sets/svm/breast_cancer.data')
 
 sc = SVMConfiguration()
 
-
+mouse1 = load_mouse1()
+cec = CEC(x=mouse1.data, k=mouse1.k)
+print cec.energy()
