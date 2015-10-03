ed210721d38b9d8cbfee5028c732f0770fc3d324
Stanislw Jastrzebski <staszek.jastrzebski@gmail.com>
2015-10-03 09:35:22 +0100

    Minor fixes for R CMD check

--- R/gmum.R
+++ R/gmum.R
@@ -1,8 +1,14 @@
+# Silences R CMD check warnings
+utils::globalVariables(c(".rs.getAnywhere.original", ".rs.getAnywhere"))
+utils::globalVariables(c("X1", "X2", "label"))
+
 #' @importFrom grDevices rainbow
 #' @importFrom graphics hist pairs par plot plot.new points title
 #' @importFrom stats na.omit prcomp predict rnorm runif update
 #' @importFrom utils data read.csv
 #' @importFrom Matrix Matrix
+#' @importFrom methods as hasArg is new
+#' @importFrom grid viewport pushViewport grid.newpage grid.layout 
 NULL
 
 # Lazy loading to allow for discovery of all files
@@ -17,6 +23,10 @@ evalqOnLoad( {
   .DollarNames.Rcpp_SVMClient <<- autocompl
   .DollarNames.Rcpp_GNGServer <<- autocompl
   .DollarNames.Rcpp_CecModel <<- autocompl
+  
+  if(!exists(".rs.getAnywhere")) {
+    .rs.getAnywhere <- NULL # Silences R CMD check warning
+  }
   
   # Workaround RStudio bug
   if(exists(".rs.getAnywhere") && !exists(".rs.getAnywhere.original")) {
--- R/gng.presets.R
+++ R/gng.presets.R
@@ -1,46 +1,3 @@
-.gng.box_point<-function(r, center, prob=-1){ 
-    point <- c()
-    
-    if(prob == -1)
-      point<-center
-    else
-      point<-c(center, prob)
-    
-    point[1:3] = point[1:3] + runif(3, min=-r/2.0, max=r/2.0)   
-    
-    point
-}
-
-
-.gng.plane_point<-function(r,center){
-  if(!hasArg(r)) r<-1.0
-  if(!hasArg(center)) center<-c(0,0,0)
-  
-  point<-center
-  
-  point[1]<-point[1]+r*runif(1.0)
-  point[2]<-point[2]+r*runif(1.0)
-  point[3]<-point[3]
-  
-  return(point)
-}
-
-.gng.sphere_point<-function(r,center){
-  if(!hasArg(r)) r<-1.0
-  if(!hasArg(center)) center<-c(0,0,0)
-  
-  alpha<-runif(1)*2*pi
-  beta<-runif(1)*pi
-  
-  point<-center
-  
-  point[1]<-point[1]+r*cos(alpha)*sin(beta)
-  point[2]<-point[2]+r*sin(alpha)*sin(beta)
-  point[3]<-point[3]+r*cos(beta)
-  
-  return(point)
-}
-
 #' Cube preset dataset
 #'
 #' @title gng.preset.cube
@@ -56,6 +13,20 @@
 #' gng <- GNG(X)
 #' 
 gng.preset.cube<-function(N, r=0.5, center=c(0.5,0.5,0.5)){
+  .gng.box_point<-function(r, center, prob=-1){ 
+    point <- c()
+    
+    if(prob == -1)
+      point<-center
+    else
+      point<-c(center, prob)
+    
+    point[1:3] = point[1:3] + runif(3, min=-r/2.0, max=r/2.0)   
+    
+    point
+}
+
+
   mat<-matrix(0,N,3)
   
   for(i in 1:N){
@@ -81,6 +52,18 @@ gng.preset.cube<-function(N, r=0.5, center=c(0.5,0.5,0.5)){
 #' gng <- GNG(X)
 #' 
 gng.preset.plane<-function(N, side=0.5, center=c(0.5,0.5,0.5)){
+.gng.plane_point<-function(r,center){
+  if(!hasArg(r)) r<-1.0
+  if(!hasArg(center)) center<-c(0,0,0)
+  
+  point<-center
+  
+  point[1]<-point[1]+r*runif(1.0)
+  point[2]<-point[2]+r*runif(1.0)
+  point[3]<-point[3]
+  
+  return(point)
+}
   mat<-matrix(0,N,3)
   
   for(i in 1:N){
@@ -106,6 +89,24 @@ gng.preset.plane<-function(N, side=0.5, center=c(0.5,0.5,0.5)){
 #' X <- gng.preset.sphere(100)
 #' gng <- GNG(X)
 gng.preset.sphere<-function(N, r=0.5, center=c(0.5,0.5,0.5)){
+.gng.sphere_point<-function(r,center){
+  if(!hasArg(r)) r<-1.0
+  if(!hasArg(center)) center<-c(0,0,0)
+  
+  alpha<-runif(1)*2*pi
+  beta<-runif(1)*pi
+  
+  point<-center
+  
+  point[1]<-point[1]+r*cos(alpha)*sin(beta)
+  point[2]<-point[2]+r*sin(alpha)*sin(beta)
+  point[3]<-point[3]+r*cos(beta)
+  
+  return(point)
+}
+
+
+
   mat<-matrix(0,N,3)
   
   for(i in 1:N){
